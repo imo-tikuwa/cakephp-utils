3bb8801b5243b3e928c7c01b958b7ccc3308318f
imo-tikuwa <imotikuwabathclean@gmail.com>
2020-10-26 20:41:33 +0900

    不要なリソース削除、他

--- src/Plugin.php
+++ src/Plugin.php
@@ -6,7 +6,6 @@ namespace Utilities;
 use Cake\Core\BasePlugin;
 use Cake\Core\PluginApplicationInterface;
 use Cake\Http\MiddlewareQueue;
-use Cake\Routing\RouteBuilder;
 
 /**
  * Plugin for Utilities
@@ -24,29 +23,6 @@ class Plugin extends BasePlugin
      */
     public function bootstrap(PluginApplicationInterface $app): void
     {
-    }
-
-    /**
-     * Add routes for the plugin.
-     *
-     * If your plugin has many routes and you would like to isolate them into a separate file,
-     * you can create `$plugin/config/routes.php` and delete this method.
-     *
-     * @param \Cake\Routing\RouteBuilder $routes The route builder to update.
-     * @return void
-     */
-    public function routes(RouteBuilder $routes): void
-    {
-        $routes->plugin(
-            'Utilities',
-            ['path' => '/utilities'],
-            function (RouteBuilder $builder) {
-                // Add custom routes here
-
-                $builder->fallbacks();
-            }
-        );
-        parent::routes($routes);
     }
 
     /**
