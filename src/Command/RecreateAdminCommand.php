a08de2ecaa2a897f0b40da5195beac8f9d1a5c77
imo-tikuwa <imotikuwabathclean@gmail.com>
2020-10-27 21:26:09 +0900

    管理者テーブルのデータについて再生成するコマンドを作成
    標準出力に平文のパスワードを通知するので表示しちゃいけないときはquietとか指定する必要あり

---
+++ src/Command/RecreateAdminCommand.php
@@ -0,0 +1,97 @@
+<?php
+declare(strict_types=1);
+
+namespace Utilities\Command;
+
+use Cake\Command\Command;
+use Cake\Console\Arguments;
+use Cake\Console\ConsoleIo;
+use Cake\Console\ConsoleOptionParser;
+use Cake\Database\Schema\TableSchema;
+use Cake\Utility\Security;
+
+/**
+ * RecreateAdmin command.
+ *
+ * トランケートして、id=1のスーパーユーザーを更新する
+ * 一般ユーザーがいたら新しいパスワードを再設定して保存しなおす
+ *
+ * .envのSECURITY_SALT不一致なんかでログインできなくなったときに使うこと
+ *
+ * @property \App\Model\Table\AdminsTable $Admins
+ */
+class RecreateAdminCommand extends Command
+{
+    /**
+     * Hook method for defining this command's option parser.
+     *
+     * @see https://book.cakephp.org/4/en/console-commands/commands.html#defining-arguments-and-options
+     * @param \Cake\Console\ConsoleOptionParser $parser The parser to be defined
+     * @return \Cake\Console\ConsoleOptionParser The built parser.
+     */
+    public function buildOptionParser(ConsoleOptionParser $parser): ConsoleOptionParser
+    {
+        $parser = parent::buildOptionParser($parser);
+        $parser->addArgument('mail', [
+            'required' => true,
+            'help' => 'Please input mail address.',
+        ]);
+        $parser->addArgument('password', [
+            'required' => true,
+            'help' => 'Please input password.',
+        ]);
+
+        return $parser;
+    }
+
+    /**
+     * Implement this method with your command's logic.
+     *
+     * @param \Cake\Console\Arguments $args The command arguments.
+     * @param \Cake\Console\ConsoleIo $io The console io
+     * @return null|void|int The exit code or null for success
+     */
+    public function execute(Arguments $args, ConsoleIo $io)
+    {
+        $this->loadModel('Admins');
+
+        // 一般ユーザー取得
+        $normal_admins = $this->Admins->find()->where([$this->Admins->aliasField('id <>') => 1])->toArray();
+
+        // トランケート
+        $sqls = (new TableSchema($this->Admins->getTable()))->truncateSql($this->Admins->getConnection());
+        foreach ($sqls as $sql) {
+            $this->Admins->getConnection()->execute($sql)->execute();
+        }
+
+        // 管理者再生成
+        $mail = $args->getArgument('mail');
+        $password = $args->getArgument('password');
+        $admin = $this->Admins->newEntity([
+            'mail' => $mail,
+            'password' => $password,
+        ]);
+        $this->Admins->save($admin);
+        $io->out('以下の情報で管理者を再生成しました。');
+        $output_data = [
+            ['admin id', 'mail', 'new password'],
+            ['1', $mail, $password]
+        ];
+        $io->helper('Table')->output($output_data);
+
+        // 一般ユーザーがいたらランダムな16桁のパスワードを設定して更新
+        if (!is_null($normal_admins) && count($normal_admins) > 0) {
+            unset($output_data[1]);
+            foreach ($normal_admins as $normal_admin) {
+                $new_password = Security::randomString(16);
+                $normal_admin = $this->Admins->patchEntity($normal_admin, [
+                    'password' => $new_password,
+                ])->setNew(true);
+                $this->Admins->save($normal_admin);
+                $output_data[] = [(string)$normal_admin->id, $normal_admin->mail, $new_password];
+            }
+            $io->out('一般ユーザーはそれぞれ以下ように更新しました。必要に応じて通知を行ってください。');
+            $io->helper('Table')->output($output_data);
+        }
+    }
+}
